3caf85b3f818d5f2778e2453db5db7ad8b41e570
PacteraKun <kun.li24@pactera.com>
2018-07-09 12:29:56 -0700

    change 'labels_int32' to 'labels_stage_1' and 'labels_stage_2'

--- detectron/modeling/fast_rcnn_heads.py
+++ detectron/modeling/fast_rcnn_heads.py
@@ -190,7 +190,7 @@ def add_cascade_rcnn_losses(model, thresh, i):
     elif i == 1:
         #get_labels(i)      
         cls_prob_stage_2, loss_cls_stage_2 = model.net.SoftmaxWithLoss(
-            ['cls_score_stage_2', 'labels_int32'], ['cls_prob_stage_2', 'loss_cls_stage_2'],
+            ['cls_score_stage_2', 'labels_stage_2'], ['cls_prob_stage_2', 'loss_cls_stage_2'],
             scale=model.GetLossScale()
         )
         loss_bbox_stage_2 = model.net.SmoothL1Loss(
@@ -208,7 +208,7 @@ def add_cascade_rcnn_losses(model, thresh, i):
     elif i == 2:
         #get_labels(i)
         cls_prob_stage_3, loss_cls_stage_3 = model.net.SoftmaxWithLoss(
-            ['cls_score_stage_3', 'labels_int32'], ['cls_prob_stage_3', 'loss_cls_stage_3'],
+            ['cls_score_stage_3', 'labels_stage_3'], ['cls_prob_stage_3', 'loss_cls_stage_3'],
             scale=model.GetLossScale()
         )
         loss_bbox_stage_3 = model.net.SmoothL1Loss(
